348a4b7b141ecd71da1339bad0e7c5a503c92550
Watcharaphol D <dangpiem@hotmail.com>
2024-07-13 17:46:53 +0700

    Create multiply-number fuction for object data

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// write code here
+let menu = {
+    width: 200,
+    height: 300,
+    title: 'My menu'
+};
+
+function multiplyNumeric(obj, num) {
+    for (let key in obj) {
+        if (isNaN(obj[key])) continue;
+        obj[key] *= num;
+    }
+}
+
+multiplyNumeric(menu, 3)
+console.log(menu);
